36fa63de700119304820f0a833002648cc91e07a
Jeremy Short <ijexah@gmail.com>
2015-05-22 02:07:23 +1000

    Woop Woop

--- matrix.h
+++ matrix.h
@@ -35,10 +35,10 @@ void set_seed(uint32_t value);
 void set_nthreads(ssize_t count);
 void set_dimensions(ssize_t width);
 
-void display(register matrix_t* matrix);
-void display_row(register matrix_t* matrix, const ssize_t row);
-void display_column(register matrix_t* matrix, const ssize_t column);
-void display_element(const matrix_t* matrix, ssize_t row, ssize_t column);
+void display(register matrix_t* matrix_obj);
+void display_row(register matrix_t* matrix_obj, const ssize_t row);
+void display_column(register matrix_t* matrix_obj, const ssize_t column);
+void display_element(const matrix_t* matrix_obj, ssize_t row, ssize_t column);
 
 /* matrix operations */
 
@@ -47,22 +47,23 @@ matrix_t* random_matrix(uint32_t seed);
 matrix_t* uniform_matrix(uint32_t value);
 matrix_t* sequence_matrix(uint32_t start, uint32_t step);
 
-matrix_t* cloned(register matrix_t* matrix);
-matrix_t* reversed(register matrix_t* matrix);
-matrix_t* transposed(register matrix_t* matrix);
+matrix_t* cloned(register matrix_t* matrix_obj);
+matrix_t* reversed(register matrix_t* matrix_obj);
+matrix_t* transposed(register matrix_t* matrix_obj);
 
-matrix_t* scalar_add(register matrix_t* matrix, const register uint32_t scalar);
-matrix_t* scalar_mul(register matrix_t* matrix, const register uint32_t scalar);
-matrix_t* matrix_add(register matrix_t* matrix_a, register matrix_t* matrix_b);
-matrix_t* matrix_mul(register matrix_t* matrix_a, register matrix_t* matrix_b);
+matrix_t* scalar_add(register matrix_t* matrix_obj, const register uint32_t scalar);
+matrix_t* scalar_mul(register matrix_t* matrix_obj, const register uint32_t scalar);
+matrix_t* matrix_add(register matrix_t* matrix_a_obj, register matrix_t* matrix_b_obj);
+matrix_t* matrix_mul(register matrix_t* matrix_a_obj, register matrix_t* matrix_b_obj);
 matrix_t* matrix_pow(matrix_t* matrix, const uint32_t exponent);
 
 /* compute operations */
 
-uint32_t get_sum(register matrix_t* matrix);
-uint32_t get_trace(register matrix_t* matrix);
-uint32_t get_minimum(register matrix_t* matrix);
-uint32_t get_maximum(register matrix_t* matrix);
-uint32_t get_frequency(register matrix_t* matrix, register uint32_t value);
+uint32_t get_sum(register matrix_t* matrix_obj);
+uint32_t get_trace(register matrix_t* matrix_obj);
+uint32_t get_minimum(register matrix_t* matrix_obj);
+uint32_t get_maximum(register matrix_t* matrix_obj);
+uint32_t get_frequency(register matrix_t* matrix_obj, register uint32_t value);
+uint32_t get_stats(register matrix_t *matrix_obj, char type, register uint32_t value);
 
 #endif
--- rand_test.c
+++ rand_test.c
@@ -1,29 +1,12 @@
 
 #include <stdio.h>
 
-/* always assuming int is at least 32 bits */
-int rand();
-
-int rseed = 0;
-
-inline void srand(int x)
-{
-	rseed = x;
-}
 
 static const unsigned b = 214013;
 static const unsigned c = 2531011;
 
 #define RAND_MAX_32 ((1U << 31) - 1)
 #define RAND_MAX ((1U << 15) - 1)
-
-inline int rand(void)
-{
-	rseed = (b * rseed + c);
-	printf("SEED: %u\n", rseed);
-    return (rseed >> 16) & 0x7FFF;
-}
-
 
 
 static inline unsigned powi(register unsigned input, register unsigned exponent)
@@ -54,12 +37,22 @@ static inline unsigned powi(register unsigned input, register unsigned exponent)
 
 static inline unsigned get_rand_set(register unsigned e)
 {
-	register unsigned total = (powi(b, e) + 1);
-	e >>= 1;
+	/*
+	unsigned total = 1;
 	while(e)
 	{
 		total *= (powi(b, e) + 1);
 		e >>= 1;
+	}
+	return total;
+	*/
+
+	unsigned total = 1;
+	e -= 1;
+	while(e < 400 && e)
+	{
+		total *= (powi(b, e) + 1);
+		e -= 1;
 	}
 	return total;
 }
@@ -77,22 +70,26 @@ static inline unsigned get_msb(register unsigned n)
 static inline unsigned get_seed(register unsigned seed, register unsigned num)
 {
 	register unsigned n = powi(2, get_msb(num));
-	return powi(b, num) * seed + c * get_rand_set(n);
+	return powi(b, num) * seed + c * get_rand_set(num);
 }
 
 int main()
 {
-	rand();
-	rand();
-	rand();
-	rand();
-	rand();
-	rand();
-	rand();
-	rand();
-	rand();
-	rand();
+	//printf("\n== %u ==\n\n", get_seed(rseed, 4));
 
-	printf("\n== %u ==\n\n", get_seed(0, 7));
+	register unsigned rseed = 0;
+
+		rseed = (b * rseed + c);
+		printf("%u\n", (rseed >> 16) & 0x7FFF);
+
+	register unsigned i = 32768 - 3;
+	while(i--)
+	{
+		rseed = (b * rseed + c);
+	    (rseed >> 16) & 0x7FFF;
+	}
+		rseed = (b * rseed + c);
+	printf("%u\n", (rseed >> 16) & 0x7FFF);
+
 	return 0;
 }
